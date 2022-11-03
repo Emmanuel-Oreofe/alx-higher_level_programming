f34a297490a56d320131d1299ca14a09053f22f8
Emmanuel-Oreofe <olaleyeemmanueloreofe@gmail.com>
2022-11-03 14:35:38 +0000

    run

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Fetches a URL."""
-import requests
+"""Fetches https://intranet.hbtn.io/status"""
 
 
-if __name__ == '__main__':
-    response = requests.get('http://0.0.0.0:5050/status')
+if __name__ == "__main__":
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+
     print('Body response:')
-    print('\t- type: {}'.format(type(response.text)))
-    print('\t- content: {}'.format(response.text))
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
